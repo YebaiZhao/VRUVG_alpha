99c14b1144ce0617175c448540e803f5f708e2d5
Yebai Zhao <zhaoyebai@gmail.com>
2017-05-04 15:19:41 -0400

    5/4/17

---
+++ Assets/Signal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal : MonoBehaviour {
+	[SerializeField]
+	private float flashInterval = 1.0f;
+	private bool turnActive = true;
+	// Use this for initialization
+	void Start () {
+		transform.Rotate (0, 90*Random.Range (0, 3),0);
+		StartCoroutine (Change90());
+	}
+
+	private IEnumerator Change90(){
+		while(turnActive){
+			transform.Rotate(transform.forward, 90);
+			yield return new WaitForSeconds(flashInterval);
+		}
+	}
+
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
