9804c8fa66b308e7d02ac623d3858d342cd48b05
Yebai Zhao <zhaoyebai@gmail.com>
2017-05-11 12:55:09 -0400

    5/8 evening

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : Singleton<Gamemanager> {
+	private float _timeRemaining;
+
+	public float TimeRemaining 
+	{
+		get { return _timeRemaining; }
+		set { _timeRemaining = value; }
+	}
+
+	public float maxGameTime = 5 * 60; // In seconds.
+	// Use this for initialization
+	void Start () {
+		TimeRemaining = maxGameTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TimeRemaining -= Time.deltaTime;
+		if(TimeRemaining<=0 || Input.GetKey("escape")){ // quit game under these circumstances
+			Application.Quit();
+		}
+	}
+}
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singletons<T> : MonoBehaviour where T: MonoBehaviour {
+public class Singleton<T> : MonoBehaviour where T: MonoBehaviour {
 
 	private static T _instance;
 
