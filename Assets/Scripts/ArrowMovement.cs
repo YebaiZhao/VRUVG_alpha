67d2bea0d99659c96ccd0226b7f5f65a88a44711
Yebai Zhao <zhaoyebai@gmail.com>
2017-09-14 16:45:01 -0400

    Cat script created
    Added a ramdon status machine, cat meow needs to be connected with
    trigger.

--- Assets/Scripts/ArrowMovement.cs
+++ Assets/Scripts/ArrowMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//Yebai
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
---
+++ Assets/yb_catScript.cs
@@ -0,0 +1,30 @@
+﻿//Yebai Zhao 9/14/17
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yb_catScript : MonoBehaviour {
+	public float Blend;
+	[SerializeField] private float StateMachineChangeTime = 7.0f;
+	private float timeNextPeriod = 0f;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim = GetComponent<Animator>();
+		if (Time.realtimeSinceStartup > timeNextPeriod) {
+			timeNextPeriod += StateMachineChangeTime;
+			anim.SetFloat("Blend", Random.Range(0f ,3f));
+
+		}
+
+
+	}
+}
